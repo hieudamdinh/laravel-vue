b95f46f61c2724bfa7b60ebdee4e5e649ed3390a
djhieuhb <djhieuhb@gmail.com>
2020-04-22 17:41:55 +0700

    'laravel-vue'

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Companies</div>
+
+                    <div class="panel-body table-responsive">
+                        <router-view name="companiesIndex"></router-view>
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
